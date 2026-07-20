cfea560ce46d9df1a089858216c350c28889674f
Japneet Singh <japneet.singh.jal@gmail.com>
2026-07-20 17:05:05 +0200

    fix cfg_path

--- SeniorStudent/SeniorStudentMORL.py
+++ SeniorStudent/SeniorStudentMORL.py
@@ -437,6 +437,8 @@ if __name__ == '__main__':
             name=run_name,
             config=base_config,
         )
+        repo_root = Path(PARENT)
+        cfg_path = repo_root / "config_orchestrator.json"
         wandb.save(str(cfg_path))
 
 
